627d4870348cc6dafebf4c49aa4825976f44b158
Chan-gu Lee <lee-chan-gu@nm-npark.com>
2018-03-21 19:06:54 +0900

    more learning graph for random ml

--- stat0003.py
+++ stat0003.py
@@ -13,6 +13,9 @@ def cal_pyx(kernel_x_y1, kernel_x_y0, py, xs):
 	px_noty1 = kernel_x_y0(xs) * (1 - py)
 	return px_y1 / (px_y1 + px_noty1)
 
+def transform_xs(x):
+	return [0, x]
+
 df_raw=pd.DataFrame({
     'A':np.random.normal(size = 4096),
     'T':np.random.randint(2, size=4096)},
@@ -39,7 +42,8 @@ kernel_x_y0.set_bandwidth(0.3)
 
 print(py, kernel_xy.integrate_box([-np.inf, df_sample.iloc[:,1].values.mean()], [np.inf, np.inf]))
 
-plt.figure()
+plt.figure(figsize=[5.5, 5.5])
+plt.suptitle('Learning Norm Probability', fontsize=16)
 xs = np.linspace(df_sample.iloc[:,0].min(), df_sample.iloc[:,0].max(), 500)
 ys = np.linspace(df_sample.iloc[:,1].min(), df_sample.iloc[:,1].max(), 50)
 # multivarian density
@@ -49,31 +53,36 @@ pos_xy = np.vstack([xx.ravel(), yy.ravel()])
 pdf_xy = kernel_xy(pos_xy).reshape(xx.shape)
 #print(pos_xy.shape, pos_xy)
 #print(pdf_xy.shape, pdf_xy)
-sns.heatmap(pdf_xy, cbar=False)
+sns.heatmap(pdf_xy, cbar=False, xticklabels=False, yticklabels=False)
 # pyx density computed by fxy(x,y)/fy(y)
 # py computed by integration p(y) for (0.5, 1.0)
 plt.subplot(2, 2, 2)
 plt.plot(xs, kernel_x_y1(xs))
 plt.title('f(T=1)')
 # pyx method2
-plt.subplot(2, 2, 3)
-pyx = cal_pyx(kernel_x_y1, kernel_x_y0, py, xs)
-#print(pyx.shape, pyx)
-plt.plot(xs, pyx.reshape(xs.shape))
-plt.title('P(T=1)')
-plt.subplot(2, 2, 4)
 learn_data = df_sample
-x_data = np.array([(0, x) for x in learn_data.iloc[:,0].values]).reshape([-1,2])
+x_data = np.array([transform_xs(x) for x in learn_data.iloc[:,0].values])
 y_data = cal_pyx(kernel_x_y1, kernel_x_y0, py, learn_data.iloc[:,0].values)
 print(y_data)
 test_data = df_raw.sample(n=1000)
-x_test = np.array([(0, x) for x in test_data.iloc[:,0].values]).reshape([-1,2])
+x_test = np.array([transform_xs(x) for x in test_data.iloc[:,0].values])
 y_test = cal_pyx(kernel_x_y1, kernel_x_y0, py, test_data.iloc[:,0].values)
 model = Sequential()
-model.add(Dense(2, activation='sigmoid', input_dim=2))
+model.add(Dense(x_data.shape[1], activation='sigmoid', input_dim=x_data.shape[1]))
+#model.add(Dense(x_data.shape[1] * 2, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['acc'])
 history=model.fit(x_data, y_data, epochs=30, verbose = 0, validation_data=(x_test, y_test))
+x_summary = np.array([transform_xs(x) for x in xs])
+y_summary = model.predict(x_summary)
+plt.subplot(2, 2, 3)
+pyx = cal_pyx(kernel_x_y1, kernel_x_y0, py, xs)
+#print(pyx.shape, pyx)
+plt.scatter(xs, pyx.reshape(xs.shape), s=1, c='b', label='sample')
+plt.scatter(xs, y_summary, s=1, c='r', label='predict')
+plt.legend()
+plt.title('P(T=1)')
+plt.subplot(2, 2, 4)
 plt.plot(history.history['loss'], label='loss')
 plt.plot(history.history['val_loss'], label='loss(test)')
 plt.legend()
